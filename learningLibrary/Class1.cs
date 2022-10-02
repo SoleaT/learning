953493cbdc172562b13e20afe0cf2c3c8db8103f
Cassandra <cassandra01@yandex.ru>
2022-10-03 01:13:54 +0300

    утипути библиотека

---
+++ learningLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace learningLibrary;
+public class myLearningClass
+{
+    public int InputNumbers()
+    {
+        int number = 0;
+        bool isConverted = false;
+        while (isConverted != true)
+        {
+            string input1 = Console.ReadLine() ?? "-r";
+            try
+            {
+                number = Convert.ToInt32(input1);
+                isConverted = true;
+            }
+            catch (FormatException)
+            {
+                isConverted = false;
+                Console.WriteLine("Неправильно задано число");
+            }
+        }
+        return number;
+    }
+}
+
