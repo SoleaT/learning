1f26f6aecf110af87dc0e03331fe94a4a12b9dd9
Cassandra <cassandra01@yandex.ru>
2022-10-08 14:49:23 +0300

    задачи для семинара7, без допов

--- HWRK-41-43-extra/Program.cs
+++ HWRK-41-43-extra/Program.cs
@@ -1,4 +1,5 @@
 ﻿using learningLibrary;
+using System.Linq;
 
 myLearningClass currentWork = new myLearningClass();
 
@@ -26,27 +27,38 @@ while (wantToContinue)
             System.Console.WriteLine("Введите числа в строку через пробел: ");
             Console.Write("Введите элементы(через пробел): ");
 
-            //тут какой из вариантов эффективней?
-            // int[] inputarray = Array.ConvertAll(Console.ReadLine().Split(separators), int.Parse); 
-            int[] inputarray = Console.ReadLine()!.Split(separators).Select(int.Parse).ToArray();
+            int[] inputarray = Array.ConvertAll(Console.ReadLine()!.Split(separators), int.Parse);
 
-            //классик
-            // size=array.Length;
-            // int count=0;
-            // for (int i=0;i<size;i++)                        
-            //     if (array[i]>0) count++;
-            // System.Console.WriteLine($"Вы ввели {count} чисел больше 0");
-
-            //неклассик. есть прогресс: я знаю, что b=>b>0 это лямбда-выражение, но что оно и как работает - всё ещё не знаю :'( 
-            int[] temparray = Array.FindAll(inputarray, b => b > 0);
-            System.Console.WriteLine($"Вы ввели {temparray.Length} чисел больше 0, от таких: {string.Join(" ", temparray)}");
+            int[] tempArray = Array.FindAll(inputarray, b => b > 0);
+            System.Console.WriteLine($"Вы ввели {tempArray.Length} чисел больше 0, от таких: {string.Join(" ", tempArray)}");
             break;
         case 2:
-            System.Console.WriteLine("Введите значения k1 b1 k2 b2 через пробел: ");
-            double[] coords = Console.ReadLine()!.Split(separators).Select(double.Parse).ToArray();
-            if (coords[0] == coords[2])
+            (double k, double b)[] coords = new (double, double)[2];
+            while (true)
             {
-                if (coords[0] == coords[1])
+                System.Console.WriteLine("Введите значения (k1,b1) (k2,b2) в таком же формате: ");
+                try
+                {
+                    coords = Console.ReadLine()!
+                             .Replace("(", "")
+                             .Replace(")", "")
+                             .Split(" ")
+                             .Select(a => a.Split(','))
+                             .Select(b => (k: double.Parse(b[0]), b: double.Parse(b[1])))
+                             .ToArray();
+                }
+                catch
+                {
+                    System.Console.WriteLine("Строка не была обработана. Заданы числа (1,2) (3,4)");
+                    coords[0] = (1, 2);
+                    coords[1] = (3, 4);
+                }
+                if (coords.GetLength(0) >= 2) break;
+                System.Console.WriteLine("Нужно ввести 4 цифры");
+            }
+            if (coords[0].k == coords[1].k)
+            {
+                if (coords[0].k == coords[0].b)
                 {
                     System.Console.WriteLine("Прямые совпадают");
                     break;
@@ -57,8 +69,8 @@ while (wantToContinue)
                     break;
                 }
             }
-            double x = (coords[3] - coords[1]) / (coords[0] - coords[2]);
-            double y = coords[2] * x + coords[3];
+            double x = (coords[1].b - coords[0].b) / (coords[0].k - coords[1].k);
+            double y = coords[1].k * x + coords[1].b;
             Console.WriteLine($"Точка пересечения имеет координаты {Math.Round(x, 2)},{Math.Round(y, 2)}");
             break;
         case 3:
@@ -70,10 +82,10 @@ while (wantToContinue)
             switch (key)
             {
                 case ConsoleKey.LeftArrow:
-                    System.Console.WriteLine($"Значит, влево. [{string.Join(",", newArray)}]->[{string.Join(",", currentWork.MoveArrayToSide(newArray, 'l'))}]");
+                    System.Console.WriteLine($"Значит, влево. [{string.Join(",", newArray)}]->[{string.Join(",", MoveArrayToSide(newArray, 'l'))}]");
                     break;
                 case ConsoleKey.RightArrow:
-                    System.Console.WriteLine($"Значит, вправо. [{string.Join(",", newArray)}]->[{string.Join(",", currentWork.MoveArrayToSide(newArray, 'r'))}]");
+                    System.Console.WriteLine($"Значит, вправо. [{string.Join(",", newArray)}]->[{string.Join(",", MoveArrayToSide(newArray, 'r'))}]");
                     break;
                 default:
                     System.Console.WriteLine("Мы таких стрелок не знаем");
@@ -82,19 +94,19 @@ while (wantToContinue)
             break;
         case 4:
             size = 8;
-            int[] newArray2 = currentWork.FillRandomArray(size, 5, 10); //да, опять заполняю массив, но ведь в первых двух задачах его не было
-            int yPosOld=Console.CursorTop,
+            int[] newArray2 = currentWork.FillRandomArray(size, 5, 10);
+            int yPosOld = Console.CursorTop,
                 yPos = yPosOld + 2;
             System.Console.WriteLine($"У нас есть массив [{string.Join(" ", newArray2)}]");
             bool checkNumber = false;
             bool weFoundIt = false;
             var r = new Random();
-            for (int i = 0; i < size - 1; i++)   //если можно сделать это короче с помощью какие-нибудь существующих методов, я бы хотела знать, как
+            for (int i = 0; i < size - 1; i++)
             {
                 if (newArray2[i] == newArray2[i + 1])
                 {
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    System.Console.WriteLine($"Найдены соседние повторяющиеся цифры на местах {i} и {i+1}");
+                    System.Console.WriteLine($"Найдены соседние повторяющиеся цифры на местах {i} и {i + 1}");
                     Console.SetCursorPosition(19 + i * 2, yPosOld);
                     Console.Write($"{newArray2[i]} {newArray2[i + 1]}");
                     Console.SetCursorPosition(0, yPos++);
@@ -104,7 +116,7 @@ while (wantToContinue)
                         int uniqnum = r.Next(0, 10);
                         if (!Array.Exists(newArray2, elem => elem == uniqnum))
                         {
-                            newArray2[i+1] = uniqnum;
+                            newArray2[i + 1] = uniqnum;
                             checkNumber = true;
                         }
                     }
@@ -135,4 +147,24 @@ while (wantToContinue)
             wantToContinue = false;
             break;
     }
+}
+
+
+
+//сдвигает массив на 1 символ
+int[] MoveArrayToSide(int[] inputArray, char parameter)
+{
+    int size = inputArray.Length;
+    int[] newArray = new int[size];
+    if (parameter == 'l')
+    {
+        Array.Copy(inputArray, 1, newArray, 0, size - 1);
+        newArray[size - 1] = inputArray[0];
+    }
+    else if (parameter == 'r')
+    {
+        Array.Copy(inputArray, 0, newArray, 1, size - 1);
+        newArray[0] = inputArray[size - 1];
+    }
+    return newArray;
 }
