ae0a71fdeff20a1f0fc243aa050739cc66ef42bc
Cassandra <cassandra01@yandex.ru>
2022-10-16 21:13:50 +0300

    исправлена ошибка. была сортировка столбоц, а не строк

--- HWRK54/Program.cs
+++ HWRK54/Program.cs
@@ -9,21 +9,25 @@ int rowCount = currentWork.InputNumbers();
 System.Console.Write("Введите количество столбцов: ");
 int colCount = currentWork.InputNumbers();
 int[,] intArray = new int[rowCount, colCount];
-currentWork.FillRandomArray(intArray, -10, 10);
+currentWork.FillRandomArray(intArray, 0, 10);
 currentWork.PrintMatrix(intArray);
 
-for (int i = 0; i < colCount; i++)
-{
-    for (int j = rowCount - 1; j >= 0; j--)
-    {
-        //  System.Console.WriteLine($"j={j},i={i}");
+
+//сортировка строк
+for (int i = 0; i < rowCount; i++)
+    for (int j = colCount-1; j >=0; j--)
         for (int k = 1; k <= j; k++)
-        {
+            if (intArray[i,k]>intArray[i,k-1])
+                (intArray[i,k-1], intArray[i,k]) = (intArray[i,k], intArray[i,k-1]);
             
-            if (intArray[k, i] > intArray[k - 1, i])
-                (intArray[k - 1, i], intArray[k, i]) = (intArray[k, i], intArray[k - 1, i]);
-        }
-    }
-}
+
+// сортировка столбцов
+// for (int i = 0; i < colCount; i++)
+//     for (int j = rowCount - 1; j >= 0; j--)
+//         for (int k = 1; k <= j; k++)
+//             if (intArray[k, i] > intArray[k - 1, i])
+//                 (intArray[k - 1, i], intArray[k, i]) = (intArray[k, i], intArray[k - 1, i]);
+
+
 System.Console.WriteLine();
 currentWork.PrintMatrix(intArray);
