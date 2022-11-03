2fecd7fd7a87a99cc001c1cceb8b9b6723d936ce
Solea <cassandra01@yandex.ru>
2022-11-03 20:26:54 +0300

    задачка с магазином и охранниками

--- peakhour/Program.cs
+++ peakhour/Program.cs
@@ -7,32 +7,65 @@ System.Console.WriteLine(@"Суть задачи:
 Выяснить, в какой промежуток времени (час) было больше всего посетителей.
 Данные вводятся парами: приход уход. Максимум 100 покупателей.");
 
-// (int h1, int h2)[] readData = new (int, int)[2];
-System.Console.WriteLine("Скок надо строчек?");
-int n=int.Parse(Console.ReadLine());
-int i = 0;
-(int h1,int h2)[] readData=new (int,int)[n];
-
+System.Console.WriteLine("Введите часы посещения, 1 посетитель в строке, часы через пробел: ");
+List<(int h1, int h2)> readData = new List<(int, int)>();
 while (true)
 {
-    readData = Console.ReadLine()
-                  .Split(" ")
-                  .Select(item => item.Split(" "))
-                  .Select(a => (h1: int.Parse(a[0]), h2: int.Parse(a[1])))
-                  .ToArray();
-    // // System.Console.WriteLine(Console.ReadKey().Key);
-    // if (readData.GetLength(0)>n) break;
-    // System.Console.WriteLine(i);
-    // if (readData[i].h1 == -1 || readData[i].h2 == -1) break;
-    
-    System.Console.WriteLine(readData);
-    i++;
-    System.Console.WriteLine(i);
-    if (i >= n) break;
+    try
+    {
+        readData.Add(ParseOneString(Console.ReadLine()));
+    }
+    catch (System.Exception)
+    {
+        System.Console.WriteLine("Ввод закончен");
+        break;
+    }
+}
+System.Console.WriteLine("Посетителей: " + readData.Count);
+
+int[] workingHours = new int[24];
+
+for (int i = 0; i < readData.Count; i++)
+    for (int j = readData[i].h1; j <= readData[i].h2; j++)
+        workingHours[j]++;
+
+
+int maxNumber = workingHours[0];
+for (int i = 0; i < workingHours.Length; i++)
+{
+    if (workingHours[i] > maxNumber)
+        maxNumber = workingHours[i];
 }
 
-// System.Console.WriteLine(readData.GetLength(0));
-// for (int j = 0; j < n; j++)
-// {
-//     System.Console.WriteLine(readData[j].h1+" "+readData[j].h2);
-// }
+string resultHours = "";
+for (int i = 0; i < workingHours.Length - 1; i++)
+{
+    if (workingHours[i] == maxNumber && workingHours[i + 1] == maxNumber)
+        resultHours += $" c {i} до {i + 1},";
+}
+
+System.Console.WriteLine("Интервалы наибольшего присутствия: {0}\b часов", resultHours);
+
+(int, int) ParseOneString(string s)
+{
+    int[] temp = new int[2];
+    try
+    {
+        temp = Array.ConvertAll(s.Split(" "), int.Parse);
+    }
+    catch (System.Exception)
+    {
+        throw new System.Exception("Ввод закончен");
+    }
+    if (temp[0]>temp[1])
+        (temp[0],temp[1])=(temp[1],temp[0]);
+    for (int i = 0; i < 2; i++)
+    {
+        if (temp[i] > 23)
+            temp[i] = 0;
+        if (temp[i] < 0)
+            temp[i] = 24 - temp[i];
+    }
+
+    return (temp[0], temp[1]);
+}
